6a5770bddb399f37b2e89e36a149ad90a305bdfc
Yurii <y.tsyhykal@gmail.com>
2024-10-03 22:19:49 +0200

    fix

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,14 +6,15 @@ export const initMongoConnection = async () => {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
     const url = env('MONGODB_URL');
-      const db = env('MONGODB_DB');
-      
-      await mongoose.connect(``)
+    const db = env('MONGODB_DB');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Node-MongoDB`,
+    );
+
+    console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-setupServer();
+const startServerWithDB = async () => {
+  await initMongoConnection();
+  setupServer();
+};
 
-
-//  mongodb+srv://YTsyhykal:c1JmoKWuh3uUi7YR@node-mongodb.5r80m.mongodb.net/contacts?retryWrites=true&w=majority&appName=Node-MongoDB
+startServerWithDB();
