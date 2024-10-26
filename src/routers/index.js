161f84d0944d1e3f3b51733a23d7abf6df70fcab
Yurii <y.tsyhykal@gmail.com>
2024-10-26 22:40:25 +0200

    fix path

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import authRouter from './auth.js';
 import contactsRouter from './contacts.js';
 const router = express.Router();
-
+router.use('/', (req, res) => {
+  res.send('Jare Jare Daze');
+});
 router.use('/auth', authRouter);
 router.use('/contacts', contactsRouter);
 
